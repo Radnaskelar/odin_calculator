f0166be118ccf736345b4a747d40549fc1928b79
Alex <aleksa.radisav1994@gmail.com>
2022-03-27 23:48:29 +0200

    display for digits added

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 //main math operators as functions
 
-//addition via rest parameters
-const addition = function(...args) {
+//addition via rest parameters = sum
+const sum = function (...args) {
     return args.reduce((previous, current) => {
         return previous + current;
     });
 }
+//addition
+const addition = (a, b) => a + b;
 
 //substraction
 const substraction = (a, b) => a - b;
@@ -19,8 +21,31 @@ const division = (a, b) => a / b;
 //operator
 const operate = function (operator, a, b) {
     //if ('+', '-', '*', '/') {}
-    return addition(a, b);
+    if (operator === addition) {
+        return addition(a, b);
+    }
+    if (operator === substraction) {
+        return substraction(a, b);
+    }
+    if (operator === multiplication) {
+        return multiplication(a, b);
+    }
+    if (operator === division) {
+        return division(a, b);
+    }
 }
 
-//display population
 const buttons = document.querySelectorAll('button');
+
+const numbers = Array.from(document.querySelector('#digits').querySelectorAll('button'));
+
+let displayValue = 0;
+
+numbers.forEach((button) => {
+    button.addEventListener('click', function (e) {
+        document.querySelector('#display').textContent += e.target.textContent;
+    });
+    return Array.from(displayValue);
+})
+
+
